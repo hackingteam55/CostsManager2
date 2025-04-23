99ef969863db02c75a951f447f2cc9570446b7ff
matei.oltean <olteanmat@gmail.com>
2025-04-23 14:25:41 +0300

    second commit

--- costmanager/costmanager/settings.py
+++ costmanager/costmanager/settings.py
@@ -16,6 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_project',
+    'users'
+
 ]
 
 MIDDLEWARE = [
