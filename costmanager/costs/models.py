1636911b6c8268cc27a13e807878a2dd06d7dc1f
matei.oltean <olteanmat@gmail.com>
2025-04-30 16:57:06 +0300

    add Product model

--- costmanager/costs/models.py
+++ costmanager/costs/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    nume = models.CharField(max_length=20)
+    magazin = models.CharField(max_length=20, null=True, blank=False)
+    pret = models.FloatField(max_length=5, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4(), unique=True, primary_key=True, editable=False)
